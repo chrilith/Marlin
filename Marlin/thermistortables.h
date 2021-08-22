8496a60060058582587b019c8a8fb132d75f6697
chrilith <chrilith@hotmail.com>
2021-08-22 20:23:11 +0200

    More thermistances for Dagoma printers

--- Marlin/thermistortables.h
+++ Marlin/thermistortables.h
@@ -89,6 +89,12 @@
 #if ANY_THERMISTOR_IS(16) // DAGOMA alternative ATC Semitec 104GT-2
   #include "thermistortable_16.h"
 #endif
+#if ANY_THERMISTOR_IS(17) // DAGOMA alternative ATC Semitec 104GT-2 - MKS_Base
+  #include "thermistortable_17.h"
+#endif
+#if ANY_THERMISTOR_IS(18) // DAGOMA alternative ATC Semitec 104GT-2 - MKS_Base_DKU001327
+  #include "thermistortable_18.h"
+#endif
 #if ANY_THERMISTOR_IS(20) // PT100 with INA826 amp on Ultimaker v2.0 electronics
   #include "thermistortable_20.h"
 #endif
